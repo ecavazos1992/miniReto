d3626d79151cb2fa01c933be49865054f1c0f686
Enrique Cavazos Alonso <enriquecavazosalonso@mac-mini-de-enrique.local>
2016-02-29 23:37:34 -0600

    Tarea MiniReto, primera version

---
+++ miniReto.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for i in 0...100 {
+    if i % 5 == 0 {
+        print ("\(i) Bingo!!!")
+    }
+    if i % 2 == 0 {
+        print ("\(i) Par!!!")
+    }else {
+        print ("\(i) Inpar!!!")
+    }
+    if i >= 30 && i <= 40 {
+        print ("\(i) Viva Switf!!!")
+    }
+}
